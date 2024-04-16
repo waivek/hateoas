2723509524a3d7ea3fbee6628587625669475bde
Vivek Bose <waivek@gmail.com>
2024-04-16 23:22:16 +0530

    (auto-commit)

--- hateoas-api.py
+++ hateoas-api.py
@@ -6,26 +6,95 @@ from flask import Flask
 from flask import redirect
 from flask import url_for
 from flask_cors import CORS
+from dbutils import Connection
 
 app = Flask(__name__)
 app.config['Access-Control-Allow-Origin'] = '*'
 CORS(app)
 
+connection = Connection('data/main.db')
+
 @app.route("/elements")
 def elements():
     return render_template("elements.html")
 
+# @app.template_filter()
+# def html_list(items):
+#     return render_template_string("""
+#         <div class="tall">
+#             {% for item in items %}
+#             <div class="box">
+#                 <pre>{{ item }}</pre>
+#             </div>
+#             {% endfor %}
+#         </div>""", items=items)
+
+@app.route("/sequences/<int:id>", methods=["DELETE"])
+def delete_sequence(id):
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM sequences WHERE id = ?", (id,))
+    connection.commit()
+    return redirect(url_for("view_sequences"))
+
+@app.route("/sequences", methods=["POST"])
+def create_sequence():
+    name = request.form.get("name")
+    description = request.form.get("description")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO sequences (name, description) VALUES (?, ?)", (name, description))
+    connection.commit()
+    return redirect(url_for("view_sequences"))
+
+
+@app.route("/view_sequences")
+def view_sequences():
+    sequences = []
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM sequences")
+    sequences = [dict(seq) for seq in cursor.fetchall()]
+
+
+    return render_template_string("""
+    {% extends "base.html" %}
+    {% block title %}View Sequences{% endblock %}
+    {% block body %}
+    <main class="mono tall">
+        <h1>View Sequences</h1>
+        {% include "nav.html" %}
+        {% for seq in sequences %}
+        <div class="box tall">
+            <pre>{{ seq }}</pre>
+            <div class="wide">
+                <form action="{{ url_for('delete_sequence', id=seq['id']) }}" method="DELETE">
+                    <button type="submit">Delete</button>
+                </form>
+            </div>
+        </div>
+        {% endfor %}
+        <div class="box tall">
+            <h3>Create Sequence</h3>
+            <form action="{{ url_for('create_sequence') }}" method="POST" class="tall">
+                <div>
+                    <input type="text" name="name" placeholder="name">
+                    <input type="text" name="description" placeholder="Description">
+                </div>
+                <div>
+                    <button type="submit">Create</button>
+                </div>
+            </form>
+        </div>
+    </main>
+    {% endblock %}""", sequences=sequences)
+
 @app.route("/")
-def index():
+def home():
     return render_template_string("""
         {% extends "base.html" %}
         {% block title %}Home{% endblock %}
         {% block body %}
         <main class="mono tall">
             <h1>Home</h1>
-            <div class="wide">
-                <a href="{{ url_for('elements') }}">Elements</a>
-            </div>
+            {% include "nav.html" %}
         </main>
         {% endblock %}
     """)
