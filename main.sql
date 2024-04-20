2acddbfdc7697eabd7b269eafb119f5cfa26a72a
Vivek Bose <waivek@gmail.com>
2024-04-20 12:34:37 +0530

    (auto-commit)

--- main.sql
+++ main.sql
@@ -2,32 +2,27 @@
 BEGIN TRANSACTION;
 
 CREATE TABLE "sequences" (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    name        TEXT    NOT NULL,
-    description TEXT    NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT NOT NULL, 
+    description TEXT NOT NULL
 );
 
 CREATE TABLE "portions" (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    sequence_id INTEGER NOT NULL,
-    title       TEXT    NOT NULL,
-    epoch       INTEGER NOT NULL,
-    duration    INTEGER NOT NULL,
-    user_id     TEXT    NOT NULL,
-    [order]     INTEGER NOT NULL,
-    FOREIGN KEY(sequence_id) REFERENCES sequences(id) ON DELETE CASCADE
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    sequence_id INTEGER NOT NULL, 
+    title TEXT NOT NULL, 
+    epoch INTEGER NOT NULL, 
+    duration INTEGER NOT NULL, 
+    user_id TEXT NOT NULL, 
+    [order] INTEGER NOT NULL, FOREIGN KEY(sequence_id) REFERENCES sequences(id) ON DELETE CASCADE
 );
 
 CREATE TABLE "portionurls" (
-    id         INTEGER PRIMARY KEY AUTOINCREMENT,
-    portion_id INTEGER NOT NULL,
-    url        TEXT    NOT NULL,
-    selected   INTEGER NOT NULL,
-    user_id    TEXT    NOT NULL,
-    FOREIGN KEY(portion_id) REFERENCES portions(id) ON DELETE CASCADE,
-    UNIQUE(portion_id, url)
-
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    portion_id INTEGER NOT NULL, 
+    url TEXT NOT NULL, 
+    selected INTEGER NOT NULL, 
+    user_id TEXT NOT NULL, FOREIGN KEY(portion_id) REFERENCES portions(id) ON DELETE CASCADE, UNIQUE(portion_id, url)
 );
-
 
 END TRANSACTION;
