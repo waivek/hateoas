3a6833b3458437bbe30c6265b0909c1a59c44225
Vivek Bose <waivek@gmail.com>
2024-07-27 11:19:09 +0530

    (auto-commit)

---
+++ models/model_attrs.py
@@ -0,0 +1,64 @@
+from typing import List, Optional
+import attrs
+
+@attrs.define
+class EmbeddedEmote:
+    id: str
+    emoteID: str
+    from_: int = attrs.field(metadata={'alias': 'from'})
+    typename: str = attrs.field(metadata={'alias': '__typename'})
+
+@attrs.define
+class CommentMessageFragment:
+    emote: Optional[EmbeddedEmote]
+    text: str
+    typename: str = attrs.field(metadata={'alias': '__typename'})
+
+@attrs.define
+class Badge:
+    id: str
+    setID: str
+    version: str
+    typename: str = attrs.field(metadata={'alias': '__typename'})
+
+@attrs.define
+class CommentMessage:
+    fragments: List[CommentMessageFragment]
+    userBadges: List[Badge]
+    userColor: Optional[str]
+    typename: str = attrs.field(metadata={'alias': '__typename'})
+
+@attrs.define
+class Commenter:
+    id: str
+    login: str
+    displayName: str
+    typename: str = attrs.field(metadata={'alias': '__typename'})
+
+@attrs.define
+class Comment:
+    id: str
+    commenter: Commenter
+    contentOffsetSeconds: int
+    createdAt: str
+    message: CommentMessage
+    typename: str = attrs.field(metadata={'alias': '__typename'})
+
+@attrs.define
+class CommentEdge:
+    cursor: str
+    node: Comment
+    typename: str = attrs.field(metadata={'alias': '__typename'})
+
+@attrs.define
+class PageInfo:
+    hasNextPage: bool
+    hasPreviousPage: bool
+    typename: str = attrs.field(metadata={'alias': '__typename'})
+
+@attrs.define
+class CommentsPage:
+    edges: List[CommentEdge]
+    pageInfo: PageInfo
+    typename: str = attrs.field(metadata={'alias': '__typename'})
+
---
+++ models/model_pydantic.py
@@ -0,0 +1,56 @@
+
+from typing import List, Optional
+from pydantic import BaseModel, Field
+
+class EmbeddedEmote(BaseModel):
+    id: str
+    emoteID: str
+    from_: int = Field(alias='from')
+    typename: str = Field(alias='__typename')
+
+class CommentMessageFragment(BaseModel):
+    emote: Optional[EmbeddedEmote]
+    text: str
+    typename: str = Field(alias='__typename')
+
+class Badge(BaseModel):
+    id: str
+    setID: str
+    version: str
+    typename: str = Field(alias='__typename')
+
+class CommentMessage(BaseModel):
+    fragments: List[CommentMessageFragment]
+    userBadges: List[Badge]
+    userColor: Optional[str]
+    typename: str = Field(alias='__typename')
+
+class Commenter(BaseModel):
+    id: str
+    login: str
+    displayName: str
+    typename: str = Field(alias='__typename')
+
+class Comment(BaseModel):
+    id: str
+    commenter: Commenter
+    contentOffsetSeconds: int
+    createdAt: str
+    message: CommentMessage
+    typename: str = Field(alias='__typename')
+
+class CommentEdge(BaseModel):
+    cursor: str
+    node: Comment
+    typename: str = Field(alias='__typename')
+
+class PageInfo(BaseModel):
+    hasNextPage: bool
+    hasPreviousPage: bool
+    typename: str = Field(alias='__typename')
+
+class CommentsPage(BaseModel):
+    edges: List[CommentEdge]
+    pageInfo: PageInfo
+    typename: str = Field(alias='__typename')
+
